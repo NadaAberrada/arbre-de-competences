259f09f9acc4cd4707a73c66d7349e847eda7964
Nada <110571178+NadaAberrada@users.noreply.github.com>
2023-09-26 15:34:36 +0100

    ajouter function done

--- prototype2/GestionStagiaire.php
+++ prototype2/GestionStagiaire.php
@@ -2,7 +2,8 @@
 include "./Stagiaire.php";
 include "./Ville.php";
 
-class GestionStagiaire{
+class GestionStagiaire
+{
     private $serverName = "localhost";
     private $username = "root";
     private $password = "";
@@ -22,9 +23,9 @@ class GestionStagiaire{
             $DB_con = "mysql:host=" . $this->serverName . ";dbname=" . $this->dbname . ";charset=" . $this->charset;
             $this->pdo = new PDO($DB_con, $this->username, $this->password);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-           
+
             return $this->pdo;
-        } catch (PDOException $e) {   
+        } catch (PDOException $e) {
             echo "Failed to connect with MySQL: " . $e->getMessage();
         }
     }
@@ -48,11 +49,51 @@ class GestionStagiaire{
             array_push($Stagiaires, $Stagiaire);
         }
         return $Stagiaires;
-
-        
     }
+    public function ajouterStagiaire($nom, $cne, $ville)
+{
+    try {
+        // Check if the intern with the same CNE already exists
+        $checkSql = "SELECT personne.Id FROM personne INNER JOIN ville ON personne.villeid = ville.id WHERE personne.CNE = :CNE";
+        $checkResult = $this->pdo->prepare($checkSql);
+        $checkResult->bindParam(':CNE', $cne, PDO::PARAM_STR);
+        $checkResult->execute();
+        $existingIntern = $checkResult->fetch(PDO::FETCH_ASSOC);
 
+        if ($existingIntern) {
+            // Intern with the same CNE already exists, you might want to handle this case
+            return false;
+        }
+
+        // Check if the ville exists or add it if it doesn't
+        $queryVille = "SELECT id FROM ville WHERE Ville = :nom_ville";
+        $stmtVille = $this->pdo->prepare($queryVille);
+        $stmtVille->bindParam(':nom_ville', $ville);
+        $stmtVille->execute();
+
+        $id_ville = $stmtVille->fetch(PDO::FETCH_ASSOC);
+
+        if (!$id_ville) {
+            // Ville doesn't exist, you might want to handle this case
+            return false;
+        }
+
+        // Insert the new intern
+        $queryPersonne = "INSERT INTO personne (nom, CNE, villeid) VALUES (:nom, :cne, :idville)";
+        $stmtPersonne = $this->pdo->prepare($queryPersonne);
+        $stmtPersonne->bindParam(':nom', $nom);
+        $stmtPersonne->bindParam(':cne', $cne);
+        $stmtPersonne->bindParam(':idville', $id_ville['id']);
+        $stmtPersonne->execute();
+
+        // Get the ID of the newly inserted intern
+        $personneId = $this->pdo->lastInsertId();
+
+        return $personneId;
+    } catch (PDOException $e) {
+        // Handle any database errors here
+        return false;
+    }
 }
 
-
-?>
+}
