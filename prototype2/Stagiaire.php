fa49b960537be6ffe0127d724c4502b1b69a4fc3
Nada <110571178+NadaAberrada@users.noreply.github.com>
2023-09-26 12:16:11 +0100

    add class stagiaire

--- prototype2/Stagiaire.php
+++ prototype2/Stagiaire.php
@@ -1,6 +1,43 @@
 <?php 
 
+class Stagiaire{
+    private $Id;
+    private $Nom;
+    private $CNE;
+    private $Villeid;
+    
+    public function getId()
+    {
+        return $this->Id;
+    }
+    public function setId($id)
+    {
+        $this->Id = $id;
+    }
+    public function getNom()
+    {
+        return $this->Nom;
+    }
+    public function setNom($nom)
+    {
+        $this->Nom = $nom;
+    }
+    public function getCNE()
+    {
+        return $this->CNE;
+    }
+    public function setCNE($CNE)
+    {
+        $this->CNE = $CNE;
+    }
+    public function getVilleid(){
+        return $this->Villeid;
+    }
+    public function setVilleid($villeid){
+        $this->Villeid = $villeid;
+    }
 
+}
 
 
 
