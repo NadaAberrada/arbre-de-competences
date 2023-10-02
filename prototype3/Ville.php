cfbd1c6bfd199765282bb464fb6ea78bac051537
Nada <110571178+NadaAberrada@users.noreply.github.com>
2023-10-02 09:48:56 +0100

    start

---
+++ prototype3/Ville.php
@@ -0,0 +1,18 @@
+<?php
+class Ville{
+   
+    private $Ville;
+
+    public function getVille()
+    {
+        return $this->Ville;
+    }
+    public function setVille($Ville)
+    {
+        $this->Ville = $Ville;
+    }
+    
+
+}
+
+?>
