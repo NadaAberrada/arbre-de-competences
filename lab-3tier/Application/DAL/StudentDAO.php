a4a3d74c116bfe4f47befe211ad4bcae9dd4fdea
Nada <110571178+NadaAberrada@users.noreply.github.com>
2023-10-03 14:58:43 +0100

    dao /student class

---
+++ lab-3tier/Application/DAL/StudentDAO.php
@@ -0,0 +1,56 @@
+<?php
+
+class StudentDAO{
+    private $db;
+    private $databaseConnectionObj;
+
+    public function __construct(){
+        $this->databaseConnectionObj = new DatabaseConnection();
+        $this->db = $this->databaseConnectionObj->connect();
+    }
+    
+
+    public function GetAllStudents()
+    {
+
+        $sql = "SELECT * FROM Student";
+        $raw_result = $this->db->prepare($sql);
+        if (!$raw_result->execute()) {
+            $raw_result = null;
+            exit();
+        }
+        $allTraineesData = $raw_result->fetchAll(PDO::FETCH_ASSOC);
+        $dataArr = [];
+        foreach ($allTraineesData as $data) {
+            $traineeInfo = new Student($data['Id'], $data['Name'], $data['Email'], $data['DateOfBirth']);
+            array_push($dataArr, $traineeInfo);
+        }
+
+        return $dataArr;
+    }
+
+
+    public function GetStudent($studentId)
+    {
+        if($studentId <= 0) {
+            // TODO: return type should be same datatype
+            return false;
+        }
+        $sql = "SELECT * FROM Student WHERE Id = :studentId";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':studentId', $studentId, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $aStudent = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($aStudent !== false) {
+            $studentObj = new Student($aStudent['Id'], $aStudent['Name'], $aStudent['Email'], $aStudent['DateOfBirth']);
+            return $studentObj;
+        }
+
+        return false;
+    }
+
+
+}
+?>
